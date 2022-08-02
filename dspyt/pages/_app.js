dee3cda7feac35a75a46b77998f303ef49a0efa6
pfed-prog <fedorovn19@gmail.com>
2022-08-02 20:38:30 +0200

    added ipfs node added lps templates

--- dspyt/pages/_app.js
+++ dspyt/pages/_app.js
@@ -5,9 +5,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Header />
-      <main className="container">
-        <Component {...pageProps} />
-      </main>
+      <Component {...pageProps} />
     </>
   );
 }
