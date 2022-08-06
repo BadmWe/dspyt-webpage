530e89798202d93fe7d26b508ed1d183ec9a6928
pfed-prog <fedorovn19@gmail.com>
2022-08-06 14:29:40 +0200

    added pagination

--- dspyt/pages/index.js
+++ dspyt/pages/index.js
@@ -28,6 +28,34 @@ export default function Home({ posts }) {
             <Post key={index} post={post} />
           ))}
         </div>
+        <nav
+          className="bg-white px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6"
+          aria-label="Pagination"
+        >
+          <div className="hidden sm:block">
+            <p className="text-sm text-gray-700">
+              Showing <span className="font-medium">{cnt + 1}</span> to{" "}
+              <span className="font-medium">{cnt + 5}</span> of{" "}
+              <span className="font-medium">{posts.length}</span> results
+            </p>
+          </div>
+          <div className="flex-1 flex justify-between sm:justify-end">
+            <a
+              onClick={() => (cnt > 5 ? setCnt(cnt - 5) : setCnt(0))}
+              className="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
+            >
+              Previous
+            </a>
+            <a
+              onClick={() =>
+                cnt + 5 > posts.length ? setCnt(cnt) : setCnt(cnt + 5)
+              }
+              className="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
+            >
+              Next
+            </a>
+          </div>
+        </nav>
       </div>
     </div>
   );
