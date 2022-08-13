af4bf68b7bf86ca393036420e737365f16066e33
pfed-prog <fedorovn19@gmail.com>
2022-08-13 14:41:52 +0200

    added redirects

--- dspyt/next.config.js
+++ dspyt/next.config.js
@@ -10,3 +10,16 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
 module.exports = withBundleAnalyzer({});
 
 module.exports = nextConfig;
+
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source:
+          "/2021/06/29/simple-telegram-bot-in-python-hosted-easily-on-heroku",
+        destination: "/simple-telegram-bot-in-python-hosted-easily-on-heroku",
+        permanent: true,
+      },
+    ];
+  },
+};
