ae358a295c155486c242077098e5b5cc84d978c7
pfed-prog <fedorovn19@gmail.com>
2022-10-01 19:42:58 +0200

    fix to ahrefs

--- dspyt/components/Tag.js
+++ dspyt/components/Tag.js
@@ -4,9 +4,9 @@ const Tag = ({ text }) => {
   text = text.split(" ").join("-");
   return (
     <Link href={`/tags/${text}`}>
-      <div className="items-center mt-1 mr-3 rounded-md bg-blue-100 font-medium uppercase px-2 py-0.5 text-xs text-blue-800 hover:text-primary-600 dark:hover:text-primary-400">
+      <a className="items-center mt-1 mr-3 rounded-md bg-blue-100 font-medium uppercase px-2 py-0.5 text-xs text-blue-800 hover:text-primary-600 dark:hover:text-primary-400">
         {text}
-      </div>
+      </a>
     </Link>
   );
 };
