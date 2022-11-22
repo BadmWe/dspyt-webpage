d9ba42f607a7685585c5a7aff93ba9ed857b5470
pfed-prog <fedorovn19@gmail.com>
2022-11-22 08:31:47 +0100

    hope it wokrs

--- dspyt/pages/_document.js
+++ dspyt/pages/_document.js
@@ -8,16 +8,10 @@ class MyDocument extends Document {
         <meta charSet="UTF-8" />
         <link rel="icon" href="/big-data-svgrepo.svg" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="msapplication-TileColor" content="#000000" />
         <meta
           name="theme-color"
           media="(prefers-color-scheme: light)"
           content="#fff"
-        />
-        <meta
-          name="theme-color"
-          media="(prefers-color-scheme: dark)"
-          content="#000"
         />
         <body className="bg-white text-black antialiased dark:bg-gray-900 dark:text-white">
           <Main />
