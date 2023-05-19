0b6d76389ea83fe7a982207957e5e9bbe4d6c31f
pfed-prog <fedorovn19@gmail.com>
2023-05-19 14:16:19 +0200

    improved style further

--- dspyt/layouts/PostLayout.js
+++ dspyt/layouts/PostLayout.js
@@ -44,19 +44,15 @@ export default function PostLayout({
         <div className="xl:divide-y xl:divide-gray-200 xl:dark:divide-gray-700">
           <header className="pt-6 xl:pb-6">
             <div className="space-y-1 text-center">
-              <dl className="space-y-10">
-                <div>
-                  <dt className="sr-only">Published on</dt>
-                  <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
-                    <time dateTime={date}>
-                      {new Date(date).toLocaleDateString(
-                        siteMetadata.locale,
-                        postDateTemplate
-                      )}
-                    </time>
-                  </dd>
-                </div>
-              </dl>
+              <dt className="sr-only">Published on</dt>
+              <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
+                <time dateTime={date}>
+                  {new Date(date).toLocaleDateString(
+                    siteMetadata.locale,
+                    postDateTemplate
+                  )}
+                </time>
+              </dd>
               <PageTitle>{title}</PageTitle>
               <ImageDspyt
                 className="mx-auto rounded-lg"
