c723260bf9b4379d8bbc108ced0d85710f46b8b0
AlexFedotovqq <alexkekfedotov@gmail.com>
2023-11-21 01:49:45 +0100

    add our team

--- dspyt/pages/index.js
+++ dspyt/pages/index.js
@@ -1,9 +1,43 @@
 import Hero from "@/components/Hero";
 import Post from "@/components/Post";
 import { getAllFilesFrontMatter, getFileBySlug } from "@/lib/mdx";
+import GitHubIcon from "@/styles/GitHubIcon";
+import Instagram from "@/styles/Instagram";
+import LinkedInIcon from "@/styles/LinkedInIcon";
+import TwitterIcon from "@/styles/TwitterIcon";
 import { StarIcon } from "@heroicons/react/20/solid";
 import Head from "next/head";
 import React, { useState } from "react";
+
+const people = [
+  {
+    name: "Dmitrii Fedotov",
+    role: "Developer,Copywriter",
+    imageUrl: "/ava.jpg",
+    twitterUrl: "https://twitter.com/DmitriFedotov",
+    linkedinUrl: "#",
+    githubUrl: "https://github.com/BadmWe/",
+    InstagramUrl: "",
+  },
+  {
+    name: "Pavel Fedotov",
+    role: "Main Developer",
+    imageUrl: "/profile.webp",
+    twitterUrl: "https://twitter.com/pfedprog",
+    linkedinUrl: "https://www.linkedin.com/in/pavel-fedotov-pinsave/",
+    githubUrl: "https://github.com/pfed-prog",
+    InstagramUrl: "",
+  },
+  {
+    name: "Alexander Fedotov",
+    role: "Design developer",
+    imageUrl: "/avasasha.webp",
+    twitterUrl: "https://twitter.com/AlexFedotovqq",
+    linkedinUrl: "#",
+    githubUrl: "https://github.com/AlexFedotovqq",
+    InstagramUrl: "",
+  },
+];
 
 export default function Home({ posts }) {
   const [email, setEmail] = useState("");
@@ -53,6 +87,86 @@ export default function Home({ posts }) {
             ))}
           </div>
         </div>
+        <div className="bg-white py-32 dark:bg-cyan-950">
+          <div className="mx-auto max-w-7xl px-6 text-center lg:px-8">
+            <div className="mx-auto max-w-2xl">
+              <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl dark:text-gray-100">
+                Meet our team
+              </h2>
+              <p className="mt-4 text-lg leading-8 text-gray-600 dark:text-gray-300">
+                We’re a dynamic group of individuals who are passionate about
+                what we do.
+              </p>
+            </div>
+            <ul
+              role="list"
+              className="mx-auto mt-20 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3"
+            >
+              {people.map((person) => (
+                <li key={person.name}>
+                  <img
+                    className="mx-auto h-56 w-56 rounded-full"
+                    src={person.imageUrl}
+                    alt=""
+                  />
+                  <h3 className="mt-6 text-base font-semibold leading-7 tracking-tight text-gray-900 dark:text-gray-100">
+                    {person.name}
+                  </h3>
+                  <p className="text-sm leading-6 text-gray-600 dark:text-gray-300">
+                    {person.role}
+                  </p>
+                  <ul role="list" className="mt-6 flex justify-center gap-x-6">
+                    <li>
+                      <span className="sr-only">Twitter</span>
+                      <a
+                        href={person.twitterUrl}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="text-gray-900 hover:text-gray-700 dark:text-gray-100 "
+                      >
+                        <TwitterIcon />
+                      </a>
+                    </li>
+                    <li>
+                      <span className="sr-only">Instagram</span>
+                      <a
+                        href={person.InstagramUrl}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="text-gray-900 hover:text-gray-700 dark:text-gray-100"
+                      >
+                        <Instagram />
+                      </a>
+                    </li>
+                    <li>
+                      <span className="sr-only">LinkedIn</span>
+                      <a
+                        href={person.linkedinUrl}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="text-gray-900 hover:text-gray-700 dark:text-gray-100"
+                      >
+                        <LinkedInIcon />
+                      </a>
+                    </li>
+                    <li>
+                      <span className="sr-only">Github</span>
+                      <a
+                        href={person.githubUrl}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="text-gray-900 hover:text-gray-700 dark:text-gray-100"
+                      >
+                        <GitHubIcon />
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+
         <div className=" grid relative mx-auto items-center justify-center">
           <div className="mt-4 text-center sm:max-w-xl">
             <h1 className="text-center mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-3xl dark:text-gray-100 sm:text-6xl">
