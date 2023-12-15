996157c418b9aae8e151d491304d823b0b6a141d
AlexFedotovqq <alexkekfedotov@gmail.com>
2023-12-15 14:27:28 +0100

    fixing code

--- dspyt/components/Post.js
+++ dspyt/components/Post.js
@@ -28,22 +28,20 @@ export default function Post({ post, slug }) {
         alt={title}
       />
       <div className="flex-1 bg-white p-6 flex flex-col justify-between">
-        <div className="flex-1">
-          <div className="flex flex-wrap">
-            {tags?.map((tag) => (
-              <Tag key={tag} text={tag} />
-            ))}
-          </div>
-          <div className="block mt-2">
-            <Link href={`/${slug}`} legacyBehavior>
-              <a className="text-xl font-semibold text-gray-900">{title}</a>
-            </Link>
-            <p className="mt-3 text-base text-gray-500">{excerpt}</p>
-          </div>
+        <div className="flex flex-wrap">
+          {tags?.map((tag) => (
+            <Tag key={tag} text={tag} />
+          ))}
+        </div>
+        <div className="block mt-2">
+          <Link href={`/${slug}`}>
+            <p className="text-xl font-semibold text-gray-900">{title}</p>
+          </Link>
+          <p className="mt-3 text-base text-gray-500">{excerpt}</p>
         </div>
         <div className="mt-6 flex items-center">
           <div className="flex-shrink-0">
-            <span className="sr-only"> {authorName}</span>
+            <span className="sr-only">{authorName}</span>
             <Image
               src={authorAvatar}
               className="h-10 w-10 rounded-full"
--- dspyt/pages/tags/[tag].js
+++ dspyt/pages/tags/[tag].js
@@ -20,10 +20,8 @@ export async function getStaticPaths() {
 
 export async function getStaticProps({ params }) {
   const allPosts = await getAllFilesFrontMatter("posts");
-  const filteredPosts = allPosts.filter(
-    (post) =>
-      post.draft !== true &&
-      post.tags.map((tag) => convertToSlug(tag)).includes(params.tag)
+  const filteredPosts = allPosts.filter((post) =>
+    post.tags.map((tag) => convertToSlug(tag)).includes(params.tag)
   );
 
   for (let i = 0; i < filteredPosts.length; i++) {
@@ -43,40 +41,40 @@ export async function getStaticProps({ params }) {
 
 export default function Tag({ posts, tag }) {
   const [page, setPage] = useState(1);
-  // Capitalize first letter and convert space to dash
-  const title = tag[0].toUpperCase() + tag.split(" ").join("-").slice(1);
+
+  const lastPost = 9 * page;
+
+  const title = tag.charAt(0).toUpperCase() + tag.split(" ").join("-").slice(1);
   return (
     <>
       <Head>
-        <title>{`Data Science with Python ${title} - DSPYT`}</title>
+        <title>{`Data Science with Python ${title}`}</title>
         <link rel="icon" href="/big-data-svgrepo.svg" />
         <meta
           name="description"
           content="Data Science with Python and blockchain DAO. We cover econometrics, python programming, blockchain technology and many more topics."
         />
-        <meta property="og:image" content="https://dspyt.com/DSPYT.png" />
+        <meta property="og:image" content="https://dspyt.com/DSPYT.webp" />
         <meta property="og:url" content={`https://dspyt.com/tags/${tag}`} />
         <meta
           property="og:title"
-          content={`Data Science with Python ${title} - DSPYT`}
+          content={`Data Science with Python ${title}`}
         />
         <meta property="og:type" content="website" />
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:site" content="@dspytdao" />
-        <meta name="twitter:creator" content="@pfedprog" />
+        <meta name="twitter:creator" content="@dspytdao" />
       </Head>
       <div className="relative max-w-7xl mx-auto mt-10">
-        <div className="text-center">
-          <h1 className="text-4xl font-extrabold text-gray-900 dark:text-gray-100 tracking-tight sm:text-5xl">
-            Dspyt blog tag: <span> {title} </span>
-          </h1>
-        </div>
+        <h1 className="text-center text-4xl font-extrabold text-gray-900 dark:text-gray-100 tracking-tight sm:text-5xl">
+          Dspyt blog tag: <span> {title} </span>
+        </h1>
         <div className="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
-          {posts.slice(0, 9 * page).map((post, index) => (
+          {posts.slice(0, lastPost).map((post, index) => (
             <Post key={index} post={post} slug={post.slug} />
           ))}
         </div>
-        {posts.length > 9 * page ? (
+        {posts.length > lastPost ? (
           <a
             onClick={() => setPage(page + 1)}
             className="relative inline-flex items-center mt-4 px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-300"
