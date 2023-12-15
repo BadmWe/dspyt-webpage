417c29ae828192982eb5085570609e981f03fc05
AlexFedotovqq <alexkekfedotov@gmail.com>
2023-12-15 08:42:11 +0100

    reduced image sizes

--- utils/convert.py
+++ utils/convert.py
@@ -2,12 +2,12 @@
 import os
 from PIL import Image
 
-DIR = os.path.join('..', 'dspyt', 'public', 'images', 'posts', 'efficientnet')
+DIR = os.path.join('..', 'dspyt', 'public', 'images', 'posts', 'time-series')
 #DIR = os.path.join('..', 'dspyt', 'public', 'authors')
 #DIR = os.path.join('..', 'dspyt', 'public')
 
 files = os.listdir(DIR)
-images = [file for file in files if file.endswith(('jpg', 'png'))]
+images = [file for file in files if file.endswith(('jpg', 'png','webp'))]
 
 for element in images:
     file_path = os.path.join(DIR, element)
