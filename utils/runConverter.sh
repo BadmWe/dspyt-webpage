78643ee8050eb89040d6bc210d7fa8ba80d2c4d4
AlexFedotovqq <alexkekfedotov@gmail.com>
2023-12-16 10:24:15 +0100

    added more echos

--- utils/runConverter.sh
+++ utils/runConverter.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+echo "DEBUG: Script directory is $SCRIPT_DIR"
+
 source $SCRIPT_DIR/venv/Scripts/activate
+
+echo "DEBUG: Virtual environment activated"
+
 python $SCRIPT_DIR/convert.py
+
+echo "DEBUG: Python script executed"
