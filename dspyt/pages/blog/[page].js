6edf8457d4a6589d5a50b80dfaffeef9f5b960b6
AlexFedotovqq <alexkekfedotov@gmail.com>
2023-12-16 09:45:01 +0100

    h1 on blog margin

--- dspyt/pages/blog/[page].js
+++ dspyt/pages/blog/[page].js
@@ -38,7 +38,10 @@ export default function Home({ posts, pagination }) {
       <div className="relative">
         <div className="relative max-w-7xl mx-auto mt-10">
           <div className="text-center">
-            <h1 className="text-4xl font-extrabold text-gray-900 dark:text-gray-100 tracking-tight sm:text-5xl">
+            <h1
+              className="text-4xl font-extrabold text-gray-900 dark:text-gray-100 tracking-tight sm:text-5xl"
+              style={{ margin: "20px" }}
+            >
               Data Science and Blockchain DSPYT Blog
             </h1>
           </div>
