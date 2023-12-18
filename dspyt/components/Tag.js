878aefb3194056f6d1f487fbe7053b0100593ebc
AlexFedotovqq <alexkekfedotov@gmail.com>
2023-12-18 18:44:44 +0100

    fix minor tag

--- dspyt/components/Tag.js
+++ dspyt/components/Tag.js
@@ -3,20 +3,22 @@ import Link from "next/link";
 
 const Tag = ({ text, paddingPx = "2px", textRounded = "rounded-md" }) => {
   return (
-    <Link
-      href={`/tags/${convertToSlug(text)}`}
-      className="items-center tracking-wide"
-    >
-      <span
-        className={
-          textRounded +
-          " bg-blue-100 font-medium uppercase text-xs text-blue-900 hover:text-primary-600 dark:hover:text-primary-400"
-        }
-        style={{ padding: paddingPx, marginRight: "4px" }}
+    <span style={{ userSelect: "none" }}>
+      <Link
+        href={`/tags/${convertToSlug(text)}`}
+        className="items-center tracking-wide"
       >
-        {text}{" "}
-      </span>
-    </Link>
+        <span
+          className={
+            textRounded +
+            " bg-blue-100 font-medium uppercase text-xs text-blue-900 hover:text-primary-600 dark:hover:text-primary-400"
+          }
+          style={{ padding: paddingPx, marginRight: "2px" }}
+        >
+          {text}
+        </span>
+      </Link>{" "}
+    </span>
   );
 };
 
