f9ed0623eb64a1afd099c30073e64435b54a4e15
AlexFedotovqq <alexkekfedotov@gmail.com>
2023-12-19 22:22:28 +0100

    fixing redirects

--- dspyt/next.config.js
+++ dspyt/next.config.js
@@ -7,20 +7,20 @@ const nextConfig = {
     return [
       {
         source:
-          "/2021/06/29/simple-telegram-bot-in-python-hosted-easily-on-heroku/",
-        destination: "/simple-telegram-bot-in-python-hosted-easily-on-heroku/",
+          "/2021/06/29/simple-telegram-bot-in-python-hosted-easily-on-heroku",
+        destination: "/simple-telegram-bot-in-python-hosted-easily-on-heroku",
         permanent: true,
       },
       {
-        source: "/2021/07/11/easy-proxy-scraper-and-proxy-usage-in-python/",
+        source: "/2021/07/11/easy-proxy-scraper-and-proxy-usage-in-python",
         destination: "/easy-proxy-scraper-and-proxy-usage-in-python",
         permanent: true,
       },
       {
         source:
-          "/2021/05/27/panel-data-econometrics-an-introduction-with-an-example-in-python/",
+          "/2021/05/27/panel-data-econometrics-an-introduction-with-an-example-in-python",
         destination:
-          "/panel-data-econometrics-an-introduction-with-an-example-in-python/",
+          "/panel-data-econometrics-an-introduction-with-an-example-in-python",
         permanent: true,
       },
       {
@@ -34,28 +34,28 @@ const nextConfig = {
         permanent: true,
       },
       {
-        source: "/page/3/",
+        source: "/page/3",
         destination: "/",
         permanent: true,
       },
       {
-        source: "/author/fedotovn19/page/3/",
+        source: "/author/fedotovn19/page/3",
         destination: "/",
         permanent: true,
       },
       {
-        source: "/tabular-playground-overfitting-solvers-club/",
+        source: "/tabular-playground-overfitting-solvers-club",
         destination: "/",
         permanent: true,
       },
       {
-        source: "/data-science-playground-solvers-club/",
+        source: "/data-science-playground-solvers-club",
         destination: "/",
         permanent: true,
       },
       {
-        source: "/honest_protocol_data_analytics/",
-        destination: "/data_analytics/",
+        source: "/honest_protocol_data_analytics",
+        destination: "/data_analytics",
         permanent: true,
       },
     ];
