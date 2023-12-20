ca66fb8dd78d86974b7855ab8adb3f86cbc61e78
BadmWe <dimakekfedotov@gmail.com>
2023-12-20 22:13:17 +0300

    text changed and mett our team

--- dspyt/pages/index.js
+++ dspyt/pages/index.js
@@ -91,7 +91,7 @@ export default function Home({ posts }) {
           <h2 className="homepage-h2">Meet our team</h2>
           <ul
             role="list"
-            className="mt-20 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3"
+            className="mx-auto mt-20 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3"
           >
             {people.map((person) => (
               <li key={person.name}>
