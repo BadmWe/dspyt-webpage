82d94b7f27fa01044d0e305284a06992eae2aab5
name <fedorovn19@gmail.com>
2024-04-29 11:39:31 +0200

    added multiple authors in a post; improving regex

--- dspyt/lib/utils/convertToSlug.js
+++ dspyt/lib/utils/convertToSlug.js
@@ -1,9 +1,9 @@
 export function convertToSlug(text) {
   const lowerCase = text.toLowerCase();
   const textNoEmptySpaces = lowerCase.replace(/([^a-zA-Z0-9])+/g, "-");
-  const textWithoutRepeatingCharacters = textNoEmptySpaces.replace(
-    /^(-)|(-$)/g,
+  const textNoleadingTrailingHyphens = textNoEmptySpaces.replace(
+    /^(?:-)|(?:-)$/g,
     ""
   );
-  return textWithoutRepeatingCharacters;
+  return textNoleadingTrailingHyphens;
 }
