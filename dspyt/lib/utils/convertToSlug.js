0e6a224a934199d76f7f2c796c4c1e6697d45089
name <fedorovn19@gmail.com>
2024-04-30 14:01:52 +0200

    refactoring regex

--- dspyt/lib/utils/convertToSlug.js
+++ dspyt/lib/utils/convertToSlug.js
@@ -1,9 +1,7 @@
 export function convertToSlug(text) {
   const lowerCase = text.toLowerCase();
   const textNoEmptySpaces = lowerCase.replace(/([^a-zA-Z0-9])+/g, "-");
-  const textNoleadingTrailingHyphens = textNoEmptySpaces.replace(
-    /^(?:-)|(?:-)$/g,
-    ""
-  );
-  return textNoleadingTrailingHyphens;
+  const textNoleadingHyphens = textNoEmptySpaces.replace(/^-/g, "");
+  const textNoTrailingHyphens = textNoleadingHyphens.replace(/-(?=$)/g, "");
+  return textNoTrailingHyphens;
 }
