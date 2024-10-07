53d4f18f5a03c286e2b4c088c51732675a523c85
name <fedorovn19@gmail.com>
2024-10-07 10:14:01 +0300

    added privacy policy

---
+++ dspyt/pages/privacy-policy.js
@@ -0,0 +1,52 @@
+export default function Policy() {
+  return (
+    <div className="relative max-w-7xl mx-auto mt-10">
+      <h1 className="dspyt-h3xl">Privacy Policy for dspyt.com</h1>
+      Effective date: October 7, 2024
+      <h2 className="mt-2">Introduction</h2>
+      Welcome to dspyt.com (the "Site"). This Privacy Policy explains how
+      dspyt.com ("we", "us", or "our") collects, uses, and discloses your
+      information when you use our website.
+      <h2 className="mt-2">Information We Collect</h2>
+      We may collect different types of information for various purposes to
+      provide and improve our Service to you. The types of information we may
+      collect include: Personal Information: When you visit our Site, we may ask
+      you to provide certain personally identifiable information that can be
+      used to contact or identify you, such as your name, email address, and
+      phone number. Log Data: Like many site operators, we collect information
+      that your browser sends whenever you visit our Site. This Log Data may
+      include information such as your computer's Internet Protocol (IP)
+      address, browser type, browser version, the pages of our Site that you
+      visit, the time and date of your visit, the time spent on those pages, and
+      other statistics. Cookies: We use cookies to collect information. You can
+      instruct your browser to refuse all cookies or to indicate when a cookie
+      is being sent. However, if you do not accept cookies, you may not be able
+      to use some portions of our Site.
+      <h2 className="mt-2">How We Use Your Information</h2>
+      We may use the information we collect for various purposes, including: To
+      provide and maintain our Site To notify you about changes to our Site To
+      allow you to participate in interactive features of our Site when you
+      choose to do so To provide customer support To gather analysis or valuable
+      information so that we can improve our Site To monitor the usage of our
+      Site To detect, prevent, and address technical issues
+      <h2 className="mt-2">Information Sharing and Disclosure</h2>
+      We do not sell, trade, or otherwise transfer your personally identifiable
+      information to outside parties. This does not include trusted third
+      parties who assist us in operating our Site, conducting our business, or
+      servicing you.
+      <h2 className="mt-2">Security</h2>
+      We take appropriate measures to ensure the security of your personal
+      information on our Site. However, no method of transmission over the
+      internet or electronic storage is 100% secure. While we strive to use
+      commercially acceptable means to protect your personal information, we
+      cannot guarantee its absolute security. Changes to This Privacy Policy We
+      may update our Privacy Policy from time to time. Any changes will be
+      posted on this page with an updated effective date. Your continued use of
+      the Site after any changes to this Privacy Policy will constitute your
+      acceptance of such changes.
+      <h2 className="mt-2">Contact Us</h2>
+      If you have any questions about this Privacy Policy, please contact us at
+      dspyt@dspyt.com.
+    </div>
+  );
+}
